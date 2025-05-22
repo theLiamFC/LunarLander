23d21643a28ef599628d53e3030fb48964b26230
bas2027 <bstettin@stanford.edu>
2025-05-21 20:25:17 -0700

    Added recover position as done in robot autonomy notes

--- src/camera.py
+++ src/camera.py
@@ -72,3 +72,55 @@ class Camera():
         _, R, t, _ = cv2.recoverPose(E, p1, p2, cameraMatrix=self.K)
         return R, t
     
+    
+    #methods used in Robot Autonomy
+    
+    def recover_pose(self, points, global_positions):
+        """recover_pose: a number of points are needed: Both 3D corresponding points and 2D image points are needed and must be in the correct order
+        
+        AS IMPLEMENTED, THIS FUNCTION ASSUMES POINTS1, POINTS2, AND GLOBAL_POSITIONS ARE IN THE SAME ORDER
+        POINTS1: nx2 matrix
+        POINTS2: nx2 matrix
+        GLOBAL_POSITIONS: nx2 matrix --> we assume that all points on the surface of the moon are at Z = 0
+        
+        NOTE: The translation will only be known up to a scale 
+              Additional information needed to recover the scale
+        """
+        n = global_positions.shape[0]
+        
+        #trying to recreate the m matrix from https://drive.google.com/drive/u/0/folders/1ycJFE38fdjRI5dL0B4rkTjIrNyveUCx5
+        uX = points[:,0:1]*global_positions[:,0:1]
+        uY = points[:,0:1]*global_positions[:,1:2]
+        vX = points[:,1:2]*global_positions[:,0:1]
+        vY = points[:,1:2]*global_positions[:,1:2]
+        u = points[:,0:1]
+        v = points[:,1:2]
+        encoded_pos = np.hstack((global_positions, np.ones((n,1)), np.zeros((n,3)), uX, uY, u, v)) #n x 9 matrix 
+        
+        encoded_pos = np.repeat(encoded_pos, 2, axis=0) #2nx9 matrix 
+        
+        row_1 = np.hstack([-1*np.ones((1,3)), np.zeros((1,3)), np.ones((1,3))])
+        row_2 = np.hstack([np.zeros((1,3)), -1*np.ones((1,3)),  np.ones((1,3))])
+        M = np.vstack((row_1, row_2))
+        M = np.tile(M, (n,1)) #2nx9 matrix 
+        
+        M = M*encoded_pos #element wise multiplication 
+        U, S, Vh = np.linalg.svd(M)
+        
+        V = Vh.T
+        H = V[:,-1].reshape(3,3) #extract last column and reshape
+        Kinv = np.linalg.inv(self.K)
+        scale = np.linalg.norm((Kinv@H)[:,0]) #take norm of the first column of K^-1 @ H
+        r0 = 1 / scale * (Kinv@H)[:,0].reshape(-1,1)
+        r1 = 1 / scale * (Kinv@H)[:,1].reshape(-1,1)
+        r2 = np.cross(r0, r1).reshape(-1, 1)
+        R = np.vstack((r0.T, r1.T, r2.T))
+        t = 1/scale (Kinv@H)[:,2].reshape(-1,1)
+        return R, t 
+        
+        
+        
+        
+        
+        
+        
