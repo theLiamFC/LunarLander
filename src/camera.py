Need to fix stuff with camera lcoation calculation

src/camera.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from crater_detector import CraterDetector
 from transformations import lla2mcmf, mcmf2lla
-from lunar_render import pixel_to_lat_lon
+from lunar_render import pixel_to_lat_lon, locate_crater
 """
 
 NOTES:
@@ -20,6 +20,7 @@ class Camera():
         self.detector = CraterDetector()
         
         if K is None:
+            
             f = 21e-3 #23mm focal length
             pixel_size = 24e-6
             focal = f/pixel_size
@@ -27,6 +28,7 @@ class Camera():
                                 [0, focal, -256], 
                                 [0, 0, 1]])
             self.focal = focal
+            self.f = f
         else:
             self.K = K #K represents the camera intrinsic matrix --> this needs to be input --> input as a 3x3
         
@@ -131,44 +133,42 @@ class Camera():
         t = 1/scale (Kinv@H)[:,2].reshape(-1,1)
         return R, t 
     
-    def get_scale(self, tile, lat, deg=True):
-        if deg:
-            lat = np.deg2rad(lat)
-            
-        predictions = self.detector.detect_craters(tile)
-        diameter = (predictions[0,2] / np.cos(lat) + predictions[0,3]) / 2 #need to scale radius
-        radius_image = diameter/2 
-        # radius_image = self.detector.estimate_crater_radius(tile)[0] #radius in pixels only get the first one
-        radius_real = radius_image / tile.image.shape[0] * tile.win * 100      #voodoo magic to get real radius
-        z = self.focal * radius_real / radius_image
-    
+    def get_scale(self, tile, alt, deg=True):
+        # predictions = self.detector.detect_craters(tile)
+        z = self.focal * alt
         return z
     
-    def get_position_global(self, tile):    
+    def get_position_global(self, tile, alt, deg=True):    
         predictions = self.detector.detect_craters(tile)
         
         image_points = predictions[0,:2].flatten()
         crater_x, crater_y = image_points
+        print("Pixel Coords in Image")
+        print(crater_x, crater_y)
         
         image_points = np.append(image_points, 1).reshape(-1,1) #put in homogeneous form
         
-        gu, gv = moon.locate_crater(tile, crater_x,crater_y)
+        print("Pixel Coords in Whole Image")
+        gu, gv = locate_crater(tile, crater_x,crater_y)
         global_points = np.array([gu,gv])
         
-        lat, lon = pixel_to_lat_lon(global_points, deg=True)
-        global_points3D = lla2mcmf(lon, lat, 0)*1e3 #converting points to m space
-        global_points3D = global_points3D.reshape(-1,1)
+        lat, lon = pixel_to_lat_lon(global_points, deg = True)
+        # global_points3D = lla2mcmf(lon, lat, 0) 
+        # global_points3D = global_points3D.reshape(-1,1)
         
+        # scale = self.get_scale(tile, alt, deg=True)
+        # print('Scale', scale)
         
-        scale = self.get_scale(tile, lat, deg=True)
-        print('Scale', scale)
-        
-        print(f"Latitude of Crater: {lat}")
-        print(f"Longitude of Crater: {lon}")
-        t = global_points3D - scale * np.linalg.inv(self.K)@image_points
-        t = mcmf2lla(t.flatten()*1e-3)
-        
-        return t #return tau as tx, ty, tz
+        # print(f"Latitude of Crater: {lat}")
+        # print(f"Longitude of Crater: {lon}")
+        # t = global_points3D - scale * np.linalg.inv(self.K)@image_points
+        # t = t.flatten()
+        # t[2] = alt
+        # t = np.array(mcmf2lla(t.flatten()))
+        # # t[2] = t[2]*1e-3
+        # t[:2] = np.deg2rad(t[:2])
+        # t[2] = alt
+        return np.array([lat, lon, alt]) #return tau as tx, ty, tz
         
 if __name__ == "__main__":
     cam = Camera()

src/lunar_render.py
@@ -61,6 +61,7 @@ class LunarRender:
         self.images = {} # dict of images in folder_path
         self.min_max = [0,0,0,0]
         self.debug = debug
+        self.verbose = True
 
         i=0
         for fname in os.listdir(self.folder_path):
@@ -249,7 +250,7 @@ class LunarRender:
         if np.isnan(render).any():
             raise ValueError(f"Requested render at {u,v,alt} out of bounds of available imaging: min {self.min_max[0:2]}, max {self.min_max[2:4]}")
         else:
-            print(f"Rendered {render.shape} image at {u,v,alt} pixels from {count} images in {self.folder_path}")
+            if self.verbose: print(f"Rendered {render.shape[0]}x{render.shape[1]} image at {u,v,alt} (px,px,m) from {count} images in {self.folder_path}")
             
             # normalize values to 0-255
             minv, maxv = render.min(), render.max()
@@ -283,34 +284,34 @@ class LunarRender:
         img = Image.fromarray(tile.image, mode='L')
         img.save(filename, format='JPEG', quality=90)
 
-    def locate_crater(self, tile, u, v):
-        """
-        Translates pixel coordinates within a given tile to global coordinates
-        in pixels. Assumes image axes aligned with the origin in the top left.
+def locate_crater(tile, u, v):
+    """
+    Translates pixel coordinates within a given tile to global coordinates
+    in pixels. Assumes image axes aligned with the origin in the top left.
 
-        Parameters
-        ----------
-        tile : Tile class
-            A custom class containing an 2d np array, centering x,y (m), and window size (m).
-        u : float
-            The pixel coordinates in x axis.
-        v : float
-            The pixel coordinates in y axis.
-        
-        Returns
-        ----------
-        u, v : float
-            The global coordinates in pixels.
-        """
-        # Calculate fractional offset from center
-        x_offset_f = (u / (tile.image.shape[0]-1)) - 0.5
-        y_offset_f = 0.5 - (v / (tile.image.shape[1]-1))
+    Parameters
+    ----------
+    tile : Tile class
+        A custom class containing an 2d np array, centering x,y (m), and window size (m).
+    u : float
+        The pixel coordinates in x axis.
+    v : float
+        The pixel coordinates in y axis.
+    
+    Returns
+    ----------
+    u, v : float
+        The global coordinates in pixels.
+    """
+    # Calculate fractional offset from center
+    x_offset_f = (u / (tile.image.shape[0]-1)) - 0.5
+    y_offset_f = 0.5 - (v / (tile.image.shape[1]-1))
 
-        # add frac * win to x,y to calc global position within tile
-        gu = tile.u + x_offset_f * tile.win
-        gv = tile.v + y_offset_f * tile.win
+    # add frac * win to x,y to calc global position within tile
+    gu = tile.u + x_offset_f * tile.win
+    gv = tile.v + y_offset_f * tile.win
 
-        return gu,gv
+    return gu,gv
 
 def pixel_to_lat_lon(pixel, pixel_scale=100, deg=False):
     u,v = pixel.flatten()

src/main.py
@@ -1,24 +1,90 @@
 from lunar_simulator import LunarSimulator
+from camera import Camera
+from trajectory_generation import generate_3d_trajectory
+from transformations import convert_traj_to_moon_fixed, mcmf_traj_to_lla
+import numpy as np
+import pandas as pd
+import EKF
+from lunar_render import LunarRender
+
 
 if __name__ == "__main__":
-    lunar_sim = LunarSimulator(
-        target,  # target landing site [x,y,z,vx=0,vy=0,vz=0] (m)
-        true_state0,  # true initial state of lander [x,y,z,vx,vy,vz] (m)
-        mu_state0,  # initial guess state of lander [x,y,z,vx,vy,vz] (m)
-        cov0,  # initial covariance of lander state
-        q_mat,  # process noise covariance matrix
-        r_mat,  # measurement noise covariance matrix
-        runtime=100,  # duration of simulation (s)
-        dt=0.1,  # delta time for simulation (s)
-        LROC_folder="WAC_ROI", # local folder containing LROC images
-        fov=45 # simulated fov of camera in degrees
-    )
+    start_LLA = (0.0, 0.0, 100000)  # Latitude, Longitude, Altitude in meters
 
-    LunarSimulator.simulate(
-        state0, 
-        seed=273, 
-        noisy=True
+    # traj.append([t, *r, *v, h, thrust_mag, *thrust_dir, phase])
+    traj_inertial = generate_3d_trajectory(
+        start_lat=start_LLA[0], start_lon=start_LLA[1], start_alt=start_LLA[2],  # 100 km above surface
+        v0_local=np.array([1600, 0, 0])  # East/North/Up in m/s
     )
+    traj_fixed = convert_traj_to_moon_fixed(traj_inertial)
 
-    LunarSimulator.plot()
+    # Lat (deg), long (deg), altitude (meters)
+    traj_fixed_LLA = mcmf_traj_to_lla(traj_fixed)
+    #print(traj_fixed_LLA)
+ 
+    # Create a DataFrame
+    df_lla = pd.DataFrame(traj_fixed_LLA, columns=["Latitude_deg", "Longitude_deg", "Altitude_m"])
+
+    # Save to CSV
+    df_lla.to_csv("traj_fixed_LLA.csv", index=False)
+
+    # Assuming traj is a NumPy array with shape (N, 13+)
+    initial_row = traj_inertial[0]
+
+    # Extract position and velocity from columns 1:4 and 4:7
+    r0_inertial = initial_row[1:4]  # x, y, z
+    v0_inertial = initial_row[4:7]  # vx, vy, vz
+    
+    cam = Camera()
+    moon = LunarRender('../WAC_ROI',debug=False)
+    moon.verbose = False
+    
+    if 0:
+        # set initial state
+        # x0 = np.array([], []) # [r v] in meters and m/s
+        # state_dim = np.size(x0)
+        meas_dim = 3
+        # sigma0 = 10000*np.eye(state_dim)
+
+        # initialize EKF
+        # ekf = EKF(state_dim, meas_dim, mu_moon = 4.9048695e12)
+        # ekf.set_initial_state(x0, sigma0)
+        # ekf.set_process_noise(np.eye(state_dim))
+        # ekf.set_measurement_noise(np.eye(meas_dim))
+        # predict
+        ekf.predict(TIME_STEP)
+
+        # update
+        ekf.update(MEASUREMENT)
+
+    measurements = np.zeros((traj_fixed_LLA.shape[0], 3))
+    
+    for i in range(traj_fixed_LLA.shape[0]):
+        lat, lon, alt = traj_fixed_LLA[i,:]
+        tile = moon.render_ll(lat=lat,lon=lon,alt=alt,deg=True)
+        measurements[i] = cam.get_position_global(tile, alt) # ouputs lat, lon, altitide (deg, deg, km) of camera position in world frame
+        print(f"True State (LLA): lat: {lat} deg, lon: {lon} deg, alt: {alt} m")
+        print(f"Estimated State (LLA): lat: {measurements[i,0]} deg, lon: {measurements[i,1]} deg, alt: {measurements[i,2]} m")
+    measurements
+    
+    # lunar_sim = LunarSimulator(
+    #     target,  # target landing site [x,y,z,vx=0,vy=0,vz=0] (m)
+    #     true_state0,  # true initial state of lander [x,y,z,vx,vy,vz] (m)
+    #     mu_state0,  # initial guess state of lander [x,y,z,vx,vy,vz] (m)
+    #     cov0,  # initial covariance of lander state
+    #     q_mat,  # process noise covariance matrix
+    #     r_mat,  # measurement noise covariance matrix
+    #     runtime=100,  # duration of simulation (s)
+    #     dt=0.1,  # delta time for simulation (s)
+    #     LROC_folder="WAC_ROI", # local folder containing LROC images
+    #     fov=45 # simulated fov of camera in degrees
+    # )
+
+    # LunarSimulator.simulate(
+    #     state0, 
+    #     seed=273, 
+    #     noisy=True
+    # )
+
+    # LunarSimulator.plot()
 